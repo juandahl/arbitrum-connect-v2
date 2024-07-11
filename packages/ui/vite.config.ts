cd146665b4928467520350027ded07eb793ff578
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2024-07-11 12:14:34 -0300

    Initial commit

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./",
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
