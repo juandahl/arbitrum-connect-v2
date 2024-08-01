292720cf1c57eac2aa8deec10cb9773ad7cc10e9
matzapata <matuzapata@gmail.com>
2024-08-01 14:20:28 +0300

    fixed

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -4,8 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import "./App.css";
 import Topbar from "./components/layout/topbar";
-import Transaction from "./components/transaction/transaction";
-import config from "./lib/wagmiConfig";
+import TransactionScreen from "./components/transaction/transaction";
+import config from "./lib/wagmi-config";
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -20,7 +20,7 @@ export default function App() {
         >
           <div className="flex flex-col w-full grow min-h-screen  bg-cover text-primary">
             <Topbar />
-            <Transaction />
+            <TransactionScreen />
           </div>
         </RainbowKitProvider>
       </QueryClientProvider>
