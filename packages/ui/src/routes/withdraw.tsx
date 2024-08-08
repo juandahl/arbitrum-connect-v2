eb5907c1c0220881c168043a6e4637ab2fc7e92a
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2024-08-08 13:09:29 -0300

    Cofirm withdrawal (#41)
    * added scripts to retrieve L1 & L2 tx estimated gas price
    
    * added fetch claim gas price
    
    * added price from gas limit function
    
    * added confirm withdrawal on review screen
    
    * updated package lock
    
    * removed many SVGs in favor of lucide-react icons
    
    * split l2withdraw & l1pushl2tx

--- packages/ui/src/routes/withdraw.tsx
+++ packages/ui/src/routes/withdraw.tsx
@@ -1,8 +1,4 @@
-import ChevronLeftIcon from "@/assets/chevron-left.svg";
 import EthIcon from "@/assets/ethereum-icon.svg";
-import StepOneIcon from "@/assets/step-one.svg";
-import StepThreeIcon from "@/assets/step-three.svg";
-import StepTwoIcon from "@/assets/step-two.svg";
 import useArbitrumBridge from "@/hooks/useArbitrumBridge";
 import { getL1TxPrice, getL1TxPriceFromGasLimit, getL2TxPrice, L1ClaimTxGasLimit, MockL1SendL2MessageTx, MockL2WithdrawTx } from "@/lib/get-tx-price";
 import { Transaction, transactionsStorageService } from "@/lib/transactions";
@@ -10,6 +6,7 @@ import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import cn from "classnames";
 import { BigNumber } from "ethers";
 import { formatEther } from "ethers/lib/utils";
+import { ChevronLeft } from 'lucide-react';
 import { useEffect, useMemo, useState } from "react";
 
 interface SearchParams {
@@ -23,7 +20,6 @@ export const Route = createFileRoute("/withdraw")({
       if (!search.amount) throw new Error("Amount is required");
       return { amount: BigNumber.from((search.amount as string).replace(/"/g, '')).toString() ?? "0" };
     } catch (e) {
-      console.log("e", e)
       return { amount: "0" };
     }
   },
@@ -35,7 +31,6 @@ function WithdrawScreen() {
 
   useEffect(() => {
     if (BigNumber.from(amountInWei).lte(0)) {
-      console.log("amountInWei", amountInWei);
       navigate({ to: "/" });
     }
   }, [amountInWei]);
@@ -46,7 +41,7 @@ function WithdrawScreen() {
   const [approvedTime, setApprovedTime] = useState<boolean>(false);
   const [loading, setLoading] = useState(false);
   const [withdrawPrice, setWithdrawPrice] = useState<string>("");
-  const [_, setConfirmWithdrawPrice] = useState<string>("");
+  const [confirmWithdrawPrice, setConfirmWithdrawPrice] = useState<string>("");
   const [claimPrice, setClaimPrice] = useState<string>("");
   const { initiateWithdraw } = useArbitrumBridge();
 
@@ -56,7 +51,6 @@ function WithdrawScreen() {
       .then((x) => {
         const tx: Transaction = {
           bridgeHash: x.l2Txhash,
-          delayedInboxHash: x.l1Txhash,
           amount: String(amountInWei),
           timestamp: Date.now(),
         };
@@ -86,7 +80,7 @@ function WithdrawScreen() {
         className="flex items-center flex-row gap-3"
         onClick={() => navigate({ to: "/" })}
       >
-        <img src={ChevronLeftIcon} />
+        <ChevronLeft size={20} />
         <div className="font-semibold text-xl">Review and confirm</div>
       </button>
 
@@ -116,9 +110,11 @@ function WithdrawScreen() {
         </div>
 
         <div className="w-full flex items-center gap-3">
-          <img src={StepOneIcon} />
+          <div className="h-5 min-w-5 mt-1 flex justify-center items-center rounded-full border-2 border-gray-800">
+            <span className="text-xs">1</span>
+          </div>
           <div className="flex flex-col md:flex-row md:justify-between w-full">
-            <span className="block text-left">Initiate Withdraw</span>
+            <span className="block text-left">Initiate Withdrawal</span>
             <div className="flex items-center flex-row gap-2">
               <span className="text-sm">{withdrawPrice.slice(0, 10)} ETH</span>
               <span className="text-neutral-400 text-sm">~ $-</span>
@@ -127,7 +123,22 @@ function WithdrawScreen() {
         </div>
 
         <div className="w-full flex items-center gap-3">
-          <img src={StepTwoIcon} />
+          <div className="h-5 min-w-5 mt-1 flex justify-center items-center rounded-full border-2 border-gray-800">
+            <span className="text-xs">2</span>
+          </div>
+          <div className="flex flex-col md:flex-row md:justify-between w-full">
+            <span className="block text-left">Confirm Withdrawal</span>
+            <div className="flex items-center flex-row gap-2">
+              <span className="text-sm">{confirmWithdrawPrice.slice(0, 10)} ETH</span>
+              <span className="text-neutral-400 text-sm">~ $-</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full flex items-center gap-3">
+          <div className="h-5 min-w-5 mt-1 flex justify-center items-center rounded-full border-2 border-gray-800">
+            <span className="text-xs">3</span>
+          </div>
           <div className="flex flex-col md:flex-row md:justify-between w-full">
             <p className="text-left">Waiting Period</p>
             <p className="text-left text-sm">~ 24 hours</p>
@@ -135,7 +146,9 @@ function WithdrawScreen() {
         </div>
 
         <div className="w-full flex items-center gap-3">
-          <img src={StepThreeIcon} />
+          <div className="h-5 min-w-5 mt-1 flex justify-center items-center rounded-full border-2 border-gray-800">
+            <span className="text-xs">4</span>
+          </div>
           <div className="flex flex-col md:flex-row md:justify-between w-full">
             <p className="text-left">Claim funds on Ethereum</p>
 
