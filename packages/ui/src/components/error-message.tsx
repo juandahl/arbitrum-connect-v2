713d2f5987dcdee64c9d5bdc11cf75169b631d53
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2024-08-15 18:32:34 -0300

    removed amount error underline

--- packages/ui/src/components/error-message.tsx
+++ packages/ui/src/components/error-message.tsx
@@ -4,7 +4,7 @@ export default function ErrorMessage({ label }: { label: string }) {
     return (
         <div className="flex pb-4 gap-2 text-[#FF0000] font-semibold text-sm">
             <InfoIcon className="text-[#FF0000] w-5 h-5" />
-            <p className="underline">{label}</p>
+            <p>{label}</p>
         </div>
     );
 }
