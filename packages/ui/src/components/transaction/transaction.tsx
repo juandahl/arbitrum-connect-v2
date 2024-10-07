e584f3199b24684569c7b9cf1bb959a52c537399
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-10-07 09:58:04 -0300

    Add WakeUp signature

--- packages/ui/src/components/transaction/transaction.tsx
+++ packages/ui/src/components/transaction/transaction.tsx
@@ -1,11 +1,11 @@
-import useArbitrumBridge from "@/hooks/useArbitrum";
-import { useState } from "react";
-import TermsModal from "../layout/terms-modal";
-import TransactionResultCard from "./result";
-import TransactionsActivity from "./activity";
-import TransactionAmount from "./amount";
-import TransactionReview from "./review";
-import { Transaction, transactionsStorageService } from "@/lib/transactions";
+import useArbitrumBridge from '@/hooks/useArbitrum';
+import { Transaction, transactionsStorageService } from '@/lib/transactions';
+import { useState } from 'react';
+import TermsModal from '../layout/terms-modal';
+import TransactionsActivity from './activity';
+import TransactionAmount from './amount';
+import TransactionResultCard from './result';
+import TransactionReview from './review';
 
 enum STEPS {
   menu,
@@ -15,10 +15,9 @@ enum STEPS {
   result,
 }
 
-
 export default function TransactionScreen() {
   const [currentStep, setCurrentStep] = useState(STEPS.menu);
-  const [amountInWei, setAmountInWei] = useState<string>("0");
+  const [amountInWei, setAmountInWei] = useState<string>('0');
   const [showModal, setShowModal] = useState(true);
   const [currentTx, setCurrentTx] = useState<Transaction | null>();
   const { initiateWithdraw } = useArbitrumBridge();
@@ -26,33 +25,32 @@ export default function TransactionScreen() {
   const onReviewSubmit = () => {
     return initiateWithdraw(amountInWei)
       .then((x) => {
-        console.log(x)
+        console.log(x);
 
         const tx: Transaction = {
           bridgeHash: x.l2Txhash,
           delayedInboxHash: x.l1Txhash,
           amount: amountInWei,
         };
-        transactionsStorageService.pushTransaction(tx)
+        transactionsStorageService.pushTransaction(tx);
         setCurrentTx(tx);
 
         setCurrentStep(STEPS.result);
       })
       .catch((e) => {
         console.error(e);
-        window.alert("Something went wrong, please try again");
+        window.alert('Something went wrong, please try again');
       });
   };
 
-
   return (
-    <div className="py-10 px-4">
+    <div className='py-10 px-4 flex-1'>
       {currentStep === STEPS.menu && (
-        <div className="flex gap-2 justify-center">
-          <button className="btn" onClick={() => setCurrentStep(STEPS.amount)}>
+        <div className='flex gap-2 justify-center'>
+          <button className='btn' onClick={() => setCurrentStep(STEPS.amount)}>
             New Tx
           </button>
-          <button className="btn" onClick={() => setCurrentStep(STEPS.list)}>
+          <button className='btn' onClick={() => setCurrentStep(STEPS.list)}>
             Last Txs
           </button>
         </div>
@@ -60,7 +58,7 @@ export default function TransactionScreen() {
       {currentStep === STEPS.list && (
         <TransactionsActivity
           setCurrentTx={(tx) => {
-            setCurrentTx(tx)
+            setCurrentTx(tx);
             setCurrentStep(STEPS.result);
           }}
           onBack={() => setCurrentStep(STEPS.menu)}
---
+++ packages/ui/src/components/wakeup-signature.tsx
@@ -0,0 +1,7 @@
+export function WakeupSignature() {
+  return (
+    <a href='https://www.wakeuplabs.io/' target='_blank' rel='noreferrer'>
+      Made with love ❤️ by WakeUp Labs
+    </a>
+  );
+}
