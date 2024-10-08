02076b48080af95676ff26d43c49c3c83ea0e789
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-10-08 08:27:49 -0300

    Add navbar with link to milestone 1

--- packages/ui/src/constants/index.ts
+++ packages/ui/src/constants/index.ts
@@ -1,1 +1,12 @@
-export const LEARN_MORE_URI = "https://docs.arbitrum.io/how-arbitrum-works/sequencer";
+import type { NavbarLink } from '@/components/layout/navbar';
+
+export const LEARN_MORE_URI = 'https://docs.arbitrum.io/how-arbitrum-works/sequencer';
+export const MILESTONE_ONE_URI = 'https://drive.google.com/file/d/1mBZLs-64t7PxTXpgJsqTmKRwsR5w5opG/view';
+
+export const NAV_LINKS: NavbarLink[] = [
+  {
+    label: 'Research',
+    to: MILESTONE_ONE_URI,
+    targetBlank: true,
+  },
+];
