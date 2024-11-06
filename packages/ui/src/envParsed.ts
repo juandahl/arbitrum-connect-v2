975ac3f67b297bcd7ea3ac55538e5634be67b91f
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2024-11-06 19:08:46 -0300

    ui env file is now generated on github workflow

--- packages/ui/src/envParsed.ts
+++ packages/ui/src/envParsed.ts
@@ -1,16 +1,16 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 // NOTE: DO NOT destructure process.env
 const env = {
   HTTPS_ETH_RPC_URL: import.meta.env.VITE_HTTPS_ETH_RPC_URL,
   HTTPS_ARB_RPC_URL: import.meta.env.VITE_HTTPS_ARB_RPC_URL,
-  IS_TESTNET: import.meta.env.VITE_IS_TESTNET, //Might be replaced for import.meta.env.dev
+  IS_TESTNET: import.meta.env.VITE_IS_TESTNET,
   RESERCH_LINK_URL: import.meta.env.VITE_RESERCH_LINK_URL,
 };
 
 const envSchema = z
   .object({
-    IS_TESTNET: z.string().transform((value) => value.toLowerCase() === 'true'),
+    IS_TESTNET: z.string().transform((value) => value.toLowerCase() === "true"),
     HTTPS_ETH_RPC_URL: z.string().url(),
     HTTPS_ARB_RPC_URL: z.string().url(),
     RESERCH_LINK_URL: z.string().url(),
